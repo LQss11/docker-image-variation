623a23cf8aa23b25ee78b095a1c80506210174ac
Salem Affes <affessalem@hotmail.fr>
2022-05-02 23:22:07 +0100

    Update image name

--- build-scripts/base.sh
+++ build-scripts/base.sh
@@ -3,20 +3,30 @@
 # This command will allow pipeline from running this script to prevent permission denied output
 # git update-index --chmod=+x .\build-scripts\anonymous-docker-build.sh
 for VARIANT in ./variants/*; do
+    # Look for dockerfile inside variants directory 
+    # To create main base image
+    # Else assign variant names in variants variable
     if [[ "$VARIANT" == *"Dockerfile"* ]]; then
         BASE=$(echo $VARIANT | cut -d "." -f3)
+        echo "Building base images $BASE..."
+        echo docker build --pull --tag $BASE --file ./variants/Dockerfile.$BASE ./base-context        
     else
-        VARIANTS+=($(echo $VARIANT | cut -d "/" -f3))
+        VARIANT=$(echo $VARIANT | cut -d "/" -f3)
+        #echo sub $VARIANT
+        sed -i "s/FROM base/FROM $BASE/g" ./variants/$VARIANT/Dockerfile 
+        sleep 3
+        sed -i "s/FROM $BASE/FROM base/g" ./variants/$VARIANT/Dockerfile 
+        #docker build . -t fifo -f <(sed "s/base/$BASE/g" ./variants/$VARIANT/Dockerfile) #>test
     fi
 done
-echo "Building base images $BASE..."
-docker build --pull --tag $BASE --file ./variants/Dockerfile.$BASE ./base-context
-for VARIANT in ${VARIANTS[@]}; do
-    #sed "s/base/$BASE/g" ./variants/$VARIANT/Dockerfile
-    echo docker build . -t fifo -f <(sed "s/base/$BASE/g" ./variants/$VARIANT/Dockerfile) >test
-    #echo "PUSHING $BASE-$VARIANT"
-    #echo docker push $BASE-$VARIANT
-done
+
+# for VAR in $VARIANTS; do
+#     #echo $VAR
+#     #sed "s/base/$BASE/g" ./variants/$VARIANT/Dockerfile
+#     #echo docker build . -t fifo -f <(sed "s/base/$BASE/g" ./variants/$VAR/Dockerfile) >test
+#     #echo "PUSHING $BASE-$VARIANT"
+#     #echo docker push $BASE-$VARIANT
+# done
 
 # echo "Pushing base images..."
 # echo "PUSHING lqss/jenkins:admin"
