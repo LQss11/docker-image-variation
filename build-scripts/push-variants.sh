a89fa627d8fe6adf7f79f4240cb85214e01f02c3
Salem Affes <affessalem@hotmail.fr>
2022-05-02 23:43:17 +0100

    Tage image to user repo

--- build-scripts/push-variants.sh
+++ build-scripts/push-variants.sh
@@ -6,10 +6,12 @@ for VARIANT in ./variants/*; do
     if [[ "$VARIANT" == *"Dockerfile"* ]]; then
         BASE=$(echo $VARIANT | cut -d "." -f3)
         echo "Pushing base image $BASE..."
+        docker image tag $BASE lqss/$BASE:latest
         docker push lqss/$BASE:latest
     else
         VARIANT=$(echo $VARIANT | cut -d "/" -f3)
         echo "Pushing variant image $VARIANT..."
+        docker image tag $BASE:$VARIANT lqss/$BASE:$VARIANT
         docker push lqss/$BASE:$VARIANT
     fi
 done
